578cf9420ef4c29d652ba2c6f914ec42b9be3270
AngeloFrog07 <angelorana82@gmail.com>
2017-06-23 16:44:53 +0100

    .java files

--- src/test/java/classPOM/browsers.java
+++ src/test/java/classPOM/browsers.java
@@ -2,7 +2,6 @@ package classPOM;
 
 import java.io.File;
 import java.io.IOException;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
